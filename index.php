fb98b1e0b57568d3fc2fa5e346823580006e7f08
LaurelineAB <laureline.agabibrac@gmail.com>
2023-07-04 10:24:54 +0000

    Début de création de compte

--- index.php
+++ index.php
@@ -2,13 +2,41 @@
 
 require "./logic/router.php";
 
-if (isset($_GET["route"]))
+if (isset($_GET['route']))
 {
-    checkroute($_GET["route"]);
+    checkroute($_GET['route']);
 }
 else
 {
     checkroute("");
+}
+
+require "models/User.php";
+// require "templates/register/_register-form.phtml";
+
+if(isset($_POST['submit']))
+{
+    if (!empty($_POST['firstName']))
+    {
+        $firstName = $_POST['firstName'];
+    }
+    if (!empty($_POST['lastName']))
+    {
+        $lastName = $_POST['lastName'];
+    }
+    if (!empty($_POST['email']))
+    {
+        $email = $_POST['email'];
+    }
+    if (!empty($_POST['password']) && !empty($_POST['confirmPassword']) && $_POST['password'] === $_POST['confirmPassword'])
+    {
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        // print_r($password);
+    }
+    $user = new User($firstName, $lastName, $email, $password);
+    // echo "<pre>";
+    // print_r($user);
+    // echo "/<pre>";
 }
 
 ?>
